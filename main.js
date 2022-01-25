03530e9fb9057d0ee2372bdbe7db1149c14ea1a4
acnow <acvarela77@gmail.com>
2022-01-25 10:37:56 -0700

    adding files

---
+++ main.js
@@ -0,0 +1,8 @@
+$('body').css("background-color", "");
+// $('.main1').css("backgrond-image", url(background1.jpg))
+$("p").css("font-size", "16px")
+// $("#parrafo").css("font-size", "40px")
+$(".lefticons").css({"background-color": "black",
+                     "opacity": .5,
+                     "border-radius": "15%",
+                    "margin": "10%"})
